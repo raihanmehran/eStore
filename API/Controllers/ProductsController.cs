900fae56a19d1fc3ec572abfd7222036a4b93a2d
Raihan Mehran <raihan.mehran@hotmail.com>
2023-07-16 22:44:17 +0200

    API: ProductsController Added

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async ValueTask<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async ValueTask<ActionResult<Product>> GetProdctById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            return product;
+        }
+    }
+}
