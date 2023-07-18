a3acbfa08a8bf70e4845035ed2d552d301412b80
Raihan Mehran <raihan.mehran@hotmail.com>
2023-07-18 15:23:40 +0200

    API: BuggyController Added

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ProblemDetails { Title = "This is a bad Request" });
+        }
+
+        [HttpGet("unauthorized-error")]
+        public ActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem 1", "This is the first error");
+            ModelState.AddModelError("Problem 2", "This the second error");
+
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+    }
+}
